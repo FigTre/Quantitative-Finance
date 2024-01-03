39519634ada23565f7dedc08997662b01459e317
Joshua Hall <97771402+FigTre@users.noreply.github.com>
2024-01-03 10:50:11 -0500

    Update rebalance-rules.R

--- R/rebalance-rules.R
+++ R/rebalance-rules.R
@@ -1,43 +1,3 @@
-#' rule to base trade size on a percentage of available equity.
-#' 
-#' This rule works with \code{\link{applyStrategy.rebalancing}} to set the
-#' maximum trade size by calling \code{\link{addPosLimit}}.  
-#' 
-#' To use it, you need to specify it as (part of) a rule of type 'rebalance'.
-#' note that  \code{\link{applyStrategy.rebalancing}} will expect a 
-#' 'rebalance_on' argument to be included in the \code{arguments=list(...)} 
-#' of the rule definition. 
-#' 
-#'
-#' 
-#' @param trade.percent max percentage of equity to allow the strategy to trade in this symbol
-#' @param longlevels numeric number of levels
-#' @param shortlevels numeric number of short levels, default longlevels 
-#' @param digits if not NULL(the default), will call \code{\link{round}} with specified number of digits
-#' @param refprice if not NULL(the default), will divide the calculated trade size by the reference price
-#' @param portfolio text name of the portfolio to place orders in, typically set automatically
-#' @param account text name of the account to fetch initial equity from, defaults to initEq in the search path
-#' @param symbol identifier of the instrument to cancel orders for, typically set automatically
-#' @param timestamp timestamp coercible to POSIXct that will be the time the order will be inserted on, typically set automatically 
-#' @param \dots any other passthrough parameters
-#' @seealso \code{\link{osMaxPos}} , 
-#' \code{\link{applyStrategy.rebalancing}}, 
-#' \code{\link{addPosLimit}}, 
-#' \code{\link{add.rule}}
-#' 
-#' @examples 
-#' # example rule definition
-#' \dontrun{
-#' add.rule(strategy.name, 'rulePctEquity',
-#'         arguments=list(rebalance_on='months',
-#'                        trade.percent=.02,
-#'                        refprice=quote(last(getPrice(mktdata)[paste('::',curIndex,sep='')])[,1]),
-#'                        digits=0
-#'         ),
-#'         type='rebalance',
-#'         label='rebalance')
-#' }
-#' @export
 rulePctEquity <- function (trade.percent=.02,
                            ...,
                            longlevels=1, 
@@ -107,20 +67,3 @@ ruleWeights <- function (weights=NULL,
             shortlevels = shortlevels)
 }
 
-#TODO weights rule that takes or calculates max position based on weights
-#TODO active weights rule that moved from current positions to target positions
-#TODO PortfolioAnalytics sizing
-#TODO LSPM sizing
-
-###############################################################################
-# R (http://r-project.org/) Quantitative Strategy Model Framework
-#
-# Copyright (c) 2009-2015
-# Peter Carl, Dirk Eddelbuettel, Brian G. Peterson, Jeffrey Ryan, and Joshua Ulrich 
-#
-# This library is distributed under the terms of the GNU Public License (GPL)
-# for full details see the file COPYING
-#
-# $Id$
-#
-###############################################################################
